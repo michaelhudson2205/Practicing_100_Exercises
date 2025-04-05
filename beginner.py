8f597023bca61775fe0a31d2103e79cd485958fb
Michael Hudson <michaelhudson2205@gmail.com>
2025-04-05 15:45:21 +1100

    Finished Exercise 6.

--- beginner.py
+++ beginner.py
@@ -21,9 +21,7 @@ print(ch)
 # ===Exercise 3===
 # Write a sequence of Python instructions to declare 2 variables x and y,
 # assigning them the values 3 and 8.5 respectively, then convert the type
-# of these variables to strings.
-# The program should display the type of these variables after conversion
-# at the end.
+# of these variables to strings. The program should display the type of these # variables after conversion at the end.
 x = 3
 y = 8.5
 print(type(x))
@@ -32,3 +30,34 @@ x = str(x)
 y = str(y)
 print(type(x))
 print(type(y))
+
+# ===Exercise 4===
+# Write a program that asks the user for their weight in kilograms, then
+# stores it in a variable. The program should display the weight entered by the user at the end.
+weight = input("What is your weight in kilograms? ")
+print("The user's weight is:", weight)
+
+# ===Exercise 5===
+# Write a sequence of Python instructions to declare a variable var and
+# assign it the value "Hello", then the program should check whether the
+# variable var is an integer or a string. If it is an integer, the program
+# should display "Interger" on the console. If it is a string, the program
+# will display "String".
+var = 16
+if type(var) == str:
+    print("The variable is a String")
+elif type(var) == int:
+    print("The variable is an Integer")
+else:
+    print("The variable is not a String or an Integer")
+
+# ===Exercise 6===
+# Write a program that declares the variable d and assigns it the value 5,
+# then checks whether this variable is greater or less than 0. If the
+# variable is greater than 0, the program should display 'Positive',
+# otherwise, it should display 'Negative'.
+d = 0
+if d >= 0:
+    print("Positive, or zero")
+else:
+    print("Negative")
