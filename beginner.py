83f242daade011b9eb689f79ac3ce3a55814cc3a
Michael Hudson <michaelhudson2205@gmail.com>
2025-04-05 12:43:41 +1100

    Finished Exercise 3.

---
+++ beginner.py
@@ -0,0 +1,34 @@
+# ===Exercise 1===
+# Write a sequence of Python instructions to declare 3 variables a,b, and
+# c, assigning them the values 1, 'France', and 36.2 respectively, then
+# display the values of thes variables on the console.
+a = 1
+b = "France"
+c = 36.2
+print(a)
+print(b)
+print(c)
+
+# ===Exercise 2===
+# Write a sequence of Python instructions to declare 1 variable ch and
+# initialize it with the value 'hello', then modify this same variable to
+# contain the value 'how are you'. The program should display the
+# content of the variable on the console after modification.
+ch = "hello"
+ch = "how are you"
+print(ch)
+
+# ===Exercise 3===
+# Write a sequence of Python instructions to declare 2 variables x and y,
+# assigning them the values 3 and 8.5 respectively, then convert the type
+# of these variables to strings.
+# The program should display the type of these variables after conversion
+# at the end.
+x = 3
+y = 8.5
+print(type(x))
+print(type(y))
+x = str(x)
+y = str(y)
+print(type(x))
+print(type(y))
