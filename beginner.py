ec025bf946b918a347c13e9e4cfa8135dfdcc5b3
Michael Hudson <michael.hudson@wnswphn.org.au>
2025-04-08 12:37:14 +1000

    Work. Finished Ex 8.

--- beginner.py
+++ beginner.py
@@ -1,4 +1,4 @@
-# ===Exercise 1===
+# >>>===Exercise 1===<<<
 # Write a sequence of Python instructions to declare 3 variables a,b, and
 # c, assigning them the values 1, 'France', and 36.2 respectively, then
 # display the values of thes variables on the console.
@@ -9,7 +9,7 @@ print(a)
 print(b)
 print(c)
 
-# ===Exercise 2===
+# >>>===Exercise 2===<<<
 # Write a sequence of Python instructions to declare 1 variable ch and
 # initialize it with the value 'hello', then modify this same variable to
 # contain the value 'how are you'. The program should display the
@@ -18,7 +18,7 @@ ch = "hello"
 ch = "how are you"
 print(ch)
 
-# ===Exercise 3===
+# >>>===Exercise 3===<<<
 # Write a sequence of Python instructions to declare 2 variables x and y,
 # assigning them the values 3 and 8.5 respectively, then convert the type
 # of these variables to strings. The program should display the type of these # variables after conversion at the end.
@@ -31,13 +31,13 @@ y = str(y)
 print(type(x))
 print(type(y))
 
-# ===Exercise 4===
+# >>>===Exercise 4===<<<
 # Write a program that asks the user for their weight in kilograms, then
 # stores it in a variable. The program should display the weight entered by the user at the end.
 weight = input("What is your weight in kilograms? ")
 print("The user's weight is:", weight)
 
-# ===Exercise 5===
+# >>>===Exercise 5===<<<
 # Write a sequence of Python instructions to declare a variable var and
 # assign it the value "Hello", then the program should check whether the
 # variable var is an integer or a string. If it is an integer, the program
@@ -51,7 +51,7 @@ elif type(var) == int:
 else:
     print("The variable is not a String or an Integer")
 
-# ===Exercise 6===
+# >>>===Exercise 6===<<<
 # Write a program that declares the variable d and assigns it the value 5,
 # then checks whether this variable is greater or less than 0. If the
 # variable is greater than 0, the program should display 'Positive',
@@ -62,7 +62,7 @@ if d >= 0:
 else:
     print("Negative")
 
-# ===Exercise 7===
+# >>>===Exercise 7===<<<
 # Write a program that asks the user for their age, then stores it in a
 # variable. The program should check whether the user is older or
 # younger than 18 years. If the user's age is greater than or equal to 18,
@@ -80,3 +80,92 @@ if age >= 18:
     print("The user is a legal adult")
 else:
     print("The user is a minor")
+
+# >>>===Exercise 8===<<<
+# Write a Python program that displays the numbers from 1 to
+# 20 inclusive on the console. Create two versions, one with
+# the for loop and the other with the while loop.
+
+# for loop
+for i in range(1, 21):
+    print(i)
+
+# while loop
+counter = 1
+while counter <= 20:
+    print(counter)
+    counter += 1
+
+# >>>===Exercise 9===<<<
+
+
+# >>>===Exercise 10===<<<
+
+
+# >>>===Exercise 11===<<<
+
+
+# >>>===Exercise 12===<<<
+
+
+# >>>===Exercise 13===<<<
+
+
+# >>>===Exercise 14===<<<
+
+
+# >>>===Exercise 15===<<<
+
+
+# >>>===Exercise 16===<<<
+
+
+# >>>===Exercise 17===<<<
+
+
+# >>>===Exercise 18===<<<
+
+
+# >>>===Exercise 19===<<<
+
+
+# >>>===Exercise 20===<<<
+
+
+# >>>===Exercise 21===<<<
+
+
+# >>>===Exercise 22===<<<
+
+
+# >>>===Exercise 23===<<<
+
+
+# >>>===Exercise 24===<<<
+
+
+# >>>===Exercise 25===<<<
+
+
+# >>>===Exercise 26===<<<
+
+
+# >>>===Exercise 27===<<<
+
+
+# >>>===Exercise 28===<<<
+
+
+# >>>===Exercise 29===<<<
+
+
+# >>>===Exercise 30===<<<
+
+
+# >>>===Exercise 31===<<<
+
+
+# >>>===Exercise 32===<<<
+
+
+# >>>===Exercise 33===<<<
